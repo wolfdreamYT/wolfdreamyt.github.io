15b74dc1322cfc89094756530f8f3e288f409876
wolfdreamYT <wolfdream226@gmail.com>
2026-01-05 16:08:52 -0500

    added fallback

---
+++ fallback.js
@@ -0,0 +1,9 @@
+// fallback.js
+const FALLBACK_URLS = [
+  "https://www.bbc.com/news/technology-64212345",
+  "https://www.wired.com/story/cybersecurity-trends-2026/",
+  "https://arstechnica.com/information-technology/2026/01/privacy-updates-2026/",
+  "https://www.techradar.com/news/top-security-threats",
+  "https://www.theguardian.com/technology/cybersecurity"
+  // add as many URLs as you want
+];
